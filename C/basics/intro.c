c02048fcea2aa21929c37f3a5967192cb02736a7
Kavyan Nayak <kavyannayak104@gmail.com>
2025-09-06 10:17:29 +0530

    Print hello world

---
+++ C/basics/intro.c
@@ -0,0 +1,30 @@
+// how to print hello world in C
+
+
+// include the standard input output library
+// header file
+
+#include <stdio.h>   // first write this line
+
+// main function -
+
+// void - data type 
+// int - data type
+// () -> Arguments
+// {} -> Body of the function
+
+void main()
+{
+    // code
+    // print hello world on the screen
+
+    // \n - new line
+    // \t - tab space
+
+    printf("Hello World");
+    printf("\t Hello \t World");
+    printf("\n Hello World");
+    printf("\t Hello World");
+    printf("\n Hello World");
+    printf("\t Hello World");
+}
